3efdb757ca9ecd0df7628cfd475b4787d79ef48b
timdekarz <tim.dekarz@generali.com>
2017-03-27 11:34:41 +0200

    INITIAL COMMIT

---
+++ src/main/java/com/generali/outbound/SecureSendingApplication.java
@@ -0,0 +1,12 @@
+package com.generali.outbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureSendingApplication.class, args);
+	}
+}
