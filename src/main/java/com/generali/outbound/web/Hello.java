dc9feba926e63e7f984693937f80bf668f3c2780
timdekarz <tim.dekarz@generali.com>
2017-03-28 11:14:37 +0200

    First Layout Try Added: - Site Layout and some Form elements

--- src/main/java/com/generali/outbound/web/Hello.java
+++ src/main/java/com/generali/outbound/web/Hello.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +32,6 @@ public class Hello {
 		model.put("message", msg);
 		model.put("title", "Hello App");
 
-		return "hello";
+		return "form";
 	}
 }
